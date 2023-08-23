4738ba1a9f50a0b273816c56f8d85f3062e95feb
Kevin Quispe <kevincarlosqa@gmail.com>
2023-08-23 06:52:14 -0500

    add: PageWrapper

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "../src/styles/PageWrapper.module.scss";
 
 const PageWrapper = ({ children }) => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1>PageWrapper</h1>
+    </div>
+  );
 };
 
 export default PageWrapper;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.scss";
+import { AnimatePresence } from "framer-motion";
+import PageWrapper from "../../components/PageWrapper";
+import Router, { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  return (
+    <AnimatePresence mode="wait">
+      <PageWrapper key={pathname}>
+        <Component {...pageProps} />
+      </PageWrapper>
+    </AnimatePresence>
+  );
 }
